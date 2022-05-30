a9ca262946e603328d1b34344434f64ea88f56a6
simonleedotcom <tjdrk2814@naver.com>
2022-05-30 11:40:52 +0900

    220530 add new/delete 동적 할당

--- p161/main.cpp
+++ p161/main.cpp
@@ -15,6 +15,18 @@ double Circle::getArea() {
 }
 void circle_print(Circle* p);
 int main() {
+	//22.05.30 new, delete 동적 할당
+	// p161 예제와 동일. 메모리 위치만 달라짐 stack -> heap.
+	Circle* p, * q;
+	p = new Circle;
+	q = new Circle(30);
+
+	cout << p->getArea() << endl << q->getArea() << endl;
+
+	delete p, q;
+
+#if 0
+	//22.05.27
 	Circle circleArray[3] = { Circle(10),Circle(20),Circle(30) }; // circle 객체 배열 
 	// 배열 초기화를 해주면 매개변수 있는 생성자를 사용할 수 있다.
 	// 배열의 각 원소 객체의 멤버 접근
@@ -30,7 +42,7 @@ int main() {
 	Circle* p;
 	p = circleArray;
 	circle_print(p);
-
+#endif
 }
 
 void circle_print(Circle* p)
